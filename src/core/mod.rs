645a23da8416872d7b10e9e42bf989665eb08fa1
한진희 <0200134hjh@gmail.com>
2025-11-17 10:26:39 +0900

    Create mod.rs

---
+++ src/core/mod.rs
@@ -0,0 +1,23 @@
+pub mod meaning_engine;
+pub mod reasoning_layer;
+pub mod memory;
+pub mod self_recreator;
+
+#[derive(Debug, Clone)]
+pub struct MeaningNode {
+    pub id: usize,
+    pub token: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct MeaningEdge {
+    pub from: usize,
+    pub to: usize,
+    pub relation: String,
+}
+
+#[derive(Debug, Clone)]
+pub struct MeaningGraph {
+    pub nodes: Vec<MeaningNode>,
+    pub edges: Vec<MeaningEdge>,
+}
